c2c5c4ae040f8ff5c679770ef74668e91ac63370
Zyril Tamargo <100452092+sadgezy@users.noreply.github.com>
2025-05-23 19:31:46 +0800

    Update next.config.mjs

--- zyril-dev-portfolio/next.config.mjs
+++ zyril-dev-portfolio/next.config.mjs
@@ -4,6 +4,8 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    basePath: "/zyril-dev-portfolio",
+    assetPrefix: "/zyril-dev-portfolio/",
 };
 
 export default nextConfig;
