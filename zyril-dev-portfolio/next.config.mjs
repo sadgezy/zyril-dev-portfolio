40c6cd29be4310b06ff66ca40e896830d718e9b2
Senen Zyril Tamargo <100452092+sadgezy@users.noreply.github.com>
2026-06-05 01:21:00 +0800

    Update next.config.mjs

--- zyril-dev-portfolio/next.config.mjs
+++ zyril-dev-portfolio/next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
-    images: {
-        unoptimized: true,
-    },
+  output: 'export',
+  
+  // Note: Because your site is hosted at github.io/zyril-dev-portfolio
+  // you will likely need to set the basePath so your CSS and images load correctly:
+  // basePath: '/zyril-dev-portfolio', 
 };
 
 export default nextConfig;
