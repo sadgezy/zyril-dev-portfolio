3e48dd1192510c58ae65d68f34fe38da794a9663
Zyril Tamargo <100452092+sadgezy@users.noreply.github.com>
2026-06-05 15:18:06 +0800

    chore: prepare project for Vercel deployment and remove gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  
-  // Add this block to disable server-side image optimization
-  images: {
-    unoptimized: true,
-  },
-
-  // Note: Keep your basePath here if you added it in the previous step!
-  basePath: '/zyril-dev-portfolio', 
+    reactStrictMode: true,
 };
 
 export default nextConfig;
